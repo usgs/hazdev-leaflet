dd7f4f1bd89fe54dc2e6d2e4f451aa69ebcb6f59
Eric Martinez <emartinez@usgs.gov>
2016-03-15 14:59:28 -0600

    Migrated in classes from usgs/earthquake-eventpages project.

--- src/leaflet/Leaflet.js
+++ src/leaflet/Leaflet.js
@@ -8,6 +8,7 @@
 
 // utilities
 require('./ArcIdentify');
+require('./UtfGrid');
 
 // controls
 require('./control/Fullscreen');
@@ -15,11 +16,13 @@ require('./control/MousePosition');
 
 // layers
 require('./layer/ArcTile');
+require('./layer/AsynchronousGeoJson');
 require('./layer/EsriGrayscale');
 require('./layer/EsriTerrain');
 require('./layer/HazardFault');
 require('./layer/HazardFault2008');
 require('./layer/HazardFault2014');
+require('./layer/MouseOverLayer');
 require('./layer/OpenAerialMap');
 require('./layer/OpenStreetMap');
 
