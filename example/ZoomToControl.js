f8771b839486b591aa95ea6c98adef455f789f7a
Hal Simpson <hasimpson@usgs.gov>
2016-04-27 10:17:47 -0600

    Refactored to follow baselayer.js example.  Added id, and other changes to reflect changes in the control.

--- example/ZoomToControl.js
+++ example/ZoomToControl.js
@@ -1,47 +1,65 @@
 /* global L */
 'use strict';
 
-var ZoomToControl = require('leaflet/control/ZoomToControl');
+require('leaflet/control/ZoomToControl');
 
-var locations = [
+var initialize = function () {
+  var locations,
+      map,
+      natgeo,
+      zoomTo;
+
+  locations = [
       {
         title:'Alaska',
+        id: 'alaska',
         bounds: [[72,-175], [50,-129]]
       },
       {
         title:'California',
+        id: 'california',
         bounds: [[42,-125], [32,-113]]
       },
       {
-        title:'Central U.S',
+        title:'Central U.S.',
+        id: 'central_us',
         bounds:[[32,-104],[40,-88]]
       },
       {
         title:'Hawaii',
+        id: 'hawaii',
         bounds: [[22,-160], [18,-154]]
       },
       {
         title:'Puerto Rico',
+        id: 'puerto_rico',
         bounds: [[20,-70], [16,-62]]
       },
       {
         title:'U.S.',
+        id: 'us',
         bounds:[[50,-125], [24.6,-65]]
       },
       {
         title:'World',
+        id: 'world',
         bounds:[[70,20],[-70,380]]
       }
     ];
 
 
-var map = new L.Map(document.querySelector('.map'), {
-    center: new L.LatLng(40.0, -105.0),
-    zoom: 3
-  });
-var zoomTo = new ZoomToControl({locations:locations});
-var natgeo = new L.TileLayer('http://server.arcgisonline.com' +
-    '/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}');
+  map = L.map(document.querySelector('.map'), {
+      center: L.latLng(40.0, -105.0),
+      zoom: 3
+    });
 
-map.addLayer(natgeo);
-map.addControl(zoomTo);
+  zoomTo = L.control.zoomToControl({locations:locations});
+  natgeo = L.tileLayer('http://server.arcgisonline.com' +
+      '/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}');
+
+  map.addLayer(natgeo);
+  map.addControl(zoomTo);
+};
+
+
+initialize();
