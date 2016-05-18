c165c66bee8c01fee78516757d4adea61e27d439
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2016-05-17 20:09:43 -0600

    added new layers

--- src/leaflet/Leaflet.js
+++ src/leaflet/Leaflet.js
@@ -18,15 +18,16 @@ require('leaflet/control/MousePosition');
 // layers
 require('leaflet/layer/ArcTile');
 require('leaflet/layer/AsynchronousGeoJson');
+require('leaflet/layer/Dark');
 require('leaflet/layer/Grayscale');
-require('leaflet/layer/Terrain');
 require('leaflet/layer/HazardFault');
 require('leaflet/layer/HazardFault2008');
 require('leaflet/layer/HazardFault2014');
 require('leaflet/layer/MouseOverLayer');
 require('leaflet/layer/Satellite');
-require('leaflet/layer/OpenStreetMap');
+require('leaflet/layer/Street');
 require('leaflet/layer/TectonicPlates');
+require('leaflet/layer/Terrain');
 require('leaflet/layer/UsFault');
 require('leaflet/layer/UsHazard');
 
