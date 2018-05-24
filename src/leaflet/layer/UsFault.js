8b74ee763b379394ec40a75889edf7ab60b9933c
Eric Martinez <emartinez@usgs.gov>
2018-05-24 13:17:12 -0600

    No dev.

--- src/leaflet/layer/UsFault.js
+++ src/leaflet/layer/UsFault.js
@@ -6,7 +6,7 @@ var MouseOverLayer = require('leaflet/layer/MouseOverLayer'),
     Util = require('hazdev-webutils/src/util/Util');
 
 
-var _FAULTS_URL = 'https://dev01-earthquake.cr.usgs.gov/basemap/tiles/faults';
+var _FAULTS_URL = 'https://earthquake.usgs.gov/basemap/tiles/faults';
 
 
 var UsFault = function (options) {
